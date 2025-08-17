257360b83cea905389bdcc7abaf21f445e239e01
tusharkumardas <tusharkumardas1125@gmail.com>
2025-08-18 00:13:58 +0530

    creating java file for establishing connection with database through MYSQL

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Tushar Kumar Das
+ */
+public class ConnectionProvider {
+     public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/grocery","root","1234");
+            return con;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+}
