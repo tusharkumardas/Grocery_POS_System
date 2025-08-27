9e53d6497d1e1aa8d7c1513a14d1881d2915d32f
tusharkumardas <tusharkumardas1125@gmail.com>
2025-08-27 17:48:04 +0530

    UPDATED PURCHASE ID GENERATOR

--- src/IDgenerator/PurchaseIDGenerator.java
+++ src/IDgenerator/PurchaseIDGenerator.java
@@ -20,7 +20,7 @@ public class PurchaseIDGenerator {
         String year = String.valueOf(java.time.Year.now().getValue());
         int nextNumber = 1;
 
-        String sql = "SELECT invoice_no FROM purchases " +
+        String sql = "SELECT purchase_no FROM purchases " +
                      "WHERE YEAR(created_at) = YEAR(CURDATE()) " +
                      "ORDER BY id DESC LIMIT 1";
 
@@ -30,7 +30,7 @@ public class PurchaseIDGenerator {
              ResultSet rs = ps.executeQuery()) {
 
             if (rs.next()) {
-                String lastID = rs.getString("purchase_id");
+                String lastID = rs.getString("purchase_no");
                 String[] parts = lastID.split("-");
                 if (parts.length == 3) {
                     nextNumber = Integer.parseInt(parts[2]) + 1;
