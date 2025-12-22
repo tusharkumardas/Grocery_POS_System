5cc9aacb760704f50a051e2641af3062ee1c342b
tusharkumardas <tusharkumardas1125@gmail.com>
2025-12-22 23:53:15 +0530

    this module holds session the session info

---
+++ src/Project/Session.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+
+/**
+ *
+ * @author Tushar Kumar Das
+ */
+public class Session {
+    public static int userId;
+    public static String username;
+    public static String role;
+} 
