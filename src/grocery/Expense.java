a5d83115506b4fa8954f3e03cff5f0db836d2299
tusharkumardas <tusharkumardas1125@gmail.com>
2025-12-22 23:54:18 +0530

    refactor the date issue

--- src/grocery/Expense.java
+++ src/grocery/Expense.java
@@ -365,7 +365,7 @@ private void clearExpenseFields() {
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, true, true, true, true
+                false, false, false, false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
